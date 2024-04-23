bd7f946c614f9567be1bc2c7fb74a102a6c45d79
Carlos Rivero Aro <carlosriveroaro7@gmail.com>
2024-04-23 12:35:31 +0200

    changed jar name

--- g2d-core/run.sh
+++ g2d-core/run.sh
@@ -1,4 +1,5 @@
 cd ..
 ./gradlew :g2d-core:createFatJar
 
-jamvm -jar g2d-core/build/libs/g2d-core-all.jar
+jamvm -version
+jamvm -Xms64m -Xmx128m -Dsun.java2d.uiScale=1.0 -jar g2d-core/build/libs/g2d-core.jar
